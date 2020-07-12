a940fdf2641b6695818756ff01cee0c363894350
Leonardo Akel Daher <leonardo.daher@usp.br>
2020-07-12 19:35:54 -0300

    Página de cadastro (funcional)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import { HomeView, ContactView, LoginView } from './views';
+import { HomeView, ContactView, LoginView, SignUpView } from './views';
 import { Header, Footer } from './components';
 import './styles.css';
 
 function App() {
   return (
     <Router>
-      <Header />
-      <Switch>
-        <Route path="/login">
-          <LoginView />
-        </Route>
-        <Route path="/contato">
-          <ContactView />
-        </Route>
-        <Route path="/">
-          <HomeView />
-        </Route>
-      </Switch>
-      <Footer />
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/cadastro">
+            <SignUpView />
+          </Route>
+          <Route path="/login">
+            <LoginView />
+          </Route>
+          <Route path="/contato">
+            <ContactView />
+          </Route>
+          <Route path="/">
+            <HomeView />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
     </Router>
   );
 }
--- frontend/src/views/index.js
+++ frontend/src/views/index.js
@@ -1,3 +1,4 @@
 export { default as HomeView } from './home';
 export { default as ContactView } from './contact';
 export { default as LoginView } from './login';
+export { default as SignUpView } from './sign-up';
