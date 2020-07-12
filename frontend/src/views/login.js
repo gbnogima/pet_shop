5ad3c45f32a1ad6eb7344bebcf20914b0ecc198f
Leonardo Akel Daher <leonardo.daher@usp.br>
2020-07-12 16:25:51 -0300

    Página de login

--- frontend/src/views/login.js
+++ frontend/src/views/login.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 function LoginView() {
-  return <h2>Login</h2>;
+  return (
+    <div>
+      <section class="page-section bg-white">
+        <div class="login-container">
+          <form action="/action_page.php">
+            <h2 class="login-title">LOGIN</h2>
+            <input class="input-text" id="fname" name="firstname" placeholder="Username"/>
+            <input class="input-text" id="lname" name="lastname" placeholder="Password"/>
+            <input class="btn-submit" value="Submit"/>
+          </form>
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default LoginView;
